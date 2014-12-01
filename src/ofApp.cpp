49537ff5196cd6edf9ff0361fa1372915408026c
jakubfiala <mu201jf@gold.ac.uk>
2014-12-01 15:12:46 +0000

    new project

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,8 +1,33 @@
+/* This is an example of how to integrate maximilain into openFrameworks,
+ including using audio received for input and audio requested for output.
+ 
+ 
+ You can copy and paste this and use it as a starting example.
+ 
+ */
+
+
 #include "ofApp.h"
+
+
+
+//-------------------------------------------------------------
+ofApp::~ofApp() {
+    
+    /*you should probably delete myData for any sample object
+     that you've created in ofApp.h*/
+    
+}
+
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+    /* some standard setup stuff*/
+    
+    ofEnableAlphaBlending();
+    ofSetupScreen();
     ofBackground(200);
+    ofSetVerticalSync(true);
     
     //create buttons
     for(int x = 0; x < 28; x++)
@@ -24,15 +49,22 @@ void ofApp::setup(){
     bpm = 120;
     newWave.isOn = false;
     
-}
+    
+    sampleRate 			= 44100; /* Sampling Rate */
+    initialBufferSize	= 512;
+    
+    
+    ofSoundStreamSetup(2,0,this, sampleRate, initialBufferSize, 4);}
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+    
+    
     for(int x = 0; x < 28; x++)
         for (int y = 0; y < 16; y++) {
             buttons[x][y].display();
@@ -40,11 +72,12 @@ void ofApp::draw(){
     play.display();
     text.drawString("RIPPLE", 900, 35);
     if (playback && fmod((ofGetElapsedTimeMillis() - startTime), getBeatTime(bpm)) <= 10.0) {
-        beat();
+        beatp();
     }
+    
 }
 
-void ofApp::beat() {
+void ofApp::beatp() {
     printf("%i",newWave.iteration);
     if (newWave.isOn) {
         if (newWave.iteration < 14) {
@@ -68,19 +101,50 @@ double ofApp::getBeatTime(double tempo) {
     return 60000/tempo;
 }
 
+
+
+//--------------------------------------------------------------
+void ofApp::audioRequested 	(float * output, int bufferSize, int nChannels){
+    
+    for (int i = 0; i < bufferSize; i++){
+        
+        
+        
+        
+    }
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::audioReceived 	(float * input, int bufferSize, int nChannels){
+    
+    
+    /* You can just grab this input and stick it in a double, then use it above to create output*/
+    
+    for (int i = 0; i < bufferSize; i++){
+        
+        /* you can also grab the data out of the arrays*/
+        
+        
+    }
+    
+}
+
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y ){
-
+    
+    
+    
 }
 
 //--------------------------------------------------------------
@@ -126,25 +190,16 @@ void ofApp::mousePressed(int mx, int my, int button){
         playback = !playback;
         startTime = ofGetElapsedTimeMillis();
     };
-
+    
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int mx, int my, int button){
-
+void ofApp::mouseReleased(int x, int y, int button){
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::windowResized(int w, int h){
-
+    
 }
 
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
-}
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,37 +1,57 @@
-#pragma once
+#ifndef _TEST_APP
+#define _TEST_APP
+
 
 #include "ofMain.h"
+#include "ofxMaxim.h"
 #include "button.h"
 
+
 class ofApp : public ofBaseApp{
-
-	public:
-		void setup();
-		void update();
-		void draw();
-
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-    protected:
-        Button buttons[28][16];
-        Button play;
-        Boolean playback;
-        ofTrueTypeFont text;
-        double startTime, bpm;
-        struct wave {
-            Boolean isOn;
-            int startX;
-            int startY;
-            int iteration;
-        } newWave;
-        void beat();
-        double getBeatTime(double tempo);
-		
+    
+public:
+    ~ofApp();/* deconsructor is very useful */
+    void setup();
+    void update();
+    void draw();
+    
+    void keyPressed  (int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void windowResized(int w, int h);
+    
+    void audioRequested 	(float * input, int bufferSize, int nChannels); /* output method */
+    void audioReceived 	(float * input, int bufferSize, int nChannels); /* input method */
+    
+    int		initialBufferSize; /* buffer size */
+    int		sampleRate;
+    
+    
+    /* stick you maximilian stuff below */
+    
+    double wave,sample,outputs[2];
+    ofxMaxiMix mymix;
+    ofxMaxiOsc sine1;
+    ofxMaxiSample beats,beat;
+    
+protected:
+    Button buttons[28][16];
+    Button play;
+    Boolean playback;
+    ofTrueTypeFont text;
+    double startTime, bpm;
+    struct wave {
+        Boolean isOn;
+        int startX;
+        int startY;
+        int iteration;
+    } newWave;
+    void beatp();
+    double getBeatTime(double tempo);
+    
 };
+
+#endif
